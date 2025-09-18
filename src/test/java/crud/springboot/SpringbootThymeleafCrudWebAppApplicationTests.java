d3829b020d006707748a6dd51b34b6ab509d92fd
Othman <badiothman13579@gmail.com>
2025-09-18 18:03:45 +0100

    Fix tests: use H2 instead of Testcontainers for CI pipeline

--- src/test/java/crud/springboot/SpringbootThymeleafCrudWebAppApplicationTests.java
+++ src/test/java/crud/springboot/SpringbootThymeleafCrudWebAppApplicationTests.java
@@ -2,31 +2,14 @@ package crud.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-@Testcontainers
 class SpringbootThymeleafCrudWebAppApplicationTests {
-
-	@Container
-	private static final MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
-			.withDatabaseName("employee_management_system")
-			.withUsername("root")
-			.withPassword("1234");
-
-	@DynamicPropertySource
-	static void dynamicProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-		registry.add("spring.datasource.username", mySQLContainer::getUsername);
-		registry.add("spring.datasource.password", mySQLContainer::getPassword);
-	}
 
 	@Test
 	void contextLoads() {
-		// This test will now pass because it connects to a temporary database
+		// Just checks if Spring context starts successfully
 	}
 }
---
+++ src/test/java/crud/springboot/service/EmployeeServiceImplTest.java
@@ -0,0 +1,7 @@
+package crud.springboot.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+}
